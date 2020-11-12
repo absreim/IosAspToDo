4a978034d6fc3f0ceac7521a2fc98db7b51c6b84
Brook Li <emcdynamicist@gmail.com>
2020-11-12 00:19:35 -0500

    Created custom class for cell and connected outlets.

---
+++ AspToDo/TodoItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  TodoItemCell.swift
+//  AspToDo
+//
+//  Created by Brook Li on 11/12/20.
+//
+
+import UIKit
+
+class TodoItemCell : UITableViewCell {
+    @IBOutlet var itemDescription: UILabel?
+    @IBOutlet var itemDone: UISwitch?
+}
