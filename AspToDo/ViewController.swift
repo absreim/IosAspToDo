293abf9ae9f19d743a154801b4f99feff9e84f55
Brook Li <emcdynamicist@gmail.com>
2020-11-14 18:33:53 -0500

    Added reference to table view to view controller and added reference to data source to table view.

--- AspToDo/ViewController.swift
+++ AspToDo/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataSource: UITableViewDataSource!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.dataSource = TodoItemTableDataSource()
+        tableView.dataSource = self.dataSource
     }
 
-
 }
-
