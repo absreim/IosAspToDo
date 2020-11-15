c9f7272bcaac58e014cf742db7c7a9dccfeea6ce
Brook Li <emcdynamicist@gmail.com>
2020-11-15 16:43:06 -0500

    Moved table data source into view controller.

--- AspToDo/ViewController.swift
+++ AspToDo/ViewController.swift
@@ -7,17 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var dataSource: UITableViewDataSource!
+    var tableData = [
+        TodoItem(description: "Learn iOS development", isDone: false),
+        TodoItem(description: "Book travel", isDone: true),
+        TodoItem(description: "Clean room", isDone: false)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.dataSource = TodoItemTableDataSource()
-        tableView.dataSource = self.dataSource
+    }
+    
+    // Return the number of rows for the table.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
     }
 
+    // Provide a cell object for each row.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Fetch a cell of the appropriate type.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath) as! TodoItemCell
+       
+        // Configure the cell’s contents.
+        cell.itemDescription!.text = tableData[indexPath.row].description
+        cell.itemDone!.isOn = tableData[indexPath.row].isDone
+           
+        return cell
+    }
+    
 }
