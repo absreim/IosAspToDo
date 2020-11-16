2177e6a9b91ee4ad0d6cb538539c1d6ee2bb9ed6
Brook Li <emcdynamicist@gmail.com>
2020-11-16 00:56:52 -0500

    Implemented retrieval of data from REST API upon loading of view.

--- AspToDo/TodoItem.swift
+++ AspToDo/TodoItem.swift
@@ -5,7 +5,19 @@
 //  Created by Brook Li on 11/12/20.
 //
 
-struct TodoItem {
-    var description: String
-    var isDone: Bool
+struct TodoItem: Codable {
+    var id: Int
+    var name: String
+    var isComplete: Bool
+    init? (json: [String: Any]) {
+        guard let id = json["id"] as? Int,
+              let name = json["name"] as? String,
+              let isComplete = json["isComplete"] as? Bool
+        else {
+            return nil
+        }
+        self.id = id
+        self.name = name
+        self.isComplete = isComplete
+    }
 }
--- AspToDo/ViewController.swift
+++ AspToDo/ViewController.swift
@@ -11,15 +11,12 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var tableData = [
-        TodoItem(description: "Learn iOS development", isDone: false),
-        TodoItem(description: "Book travel", isDone: true),
-        TodoItem(description: "Clean room", isDone: false)
-    ]
+    var tableData: [TodoItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getDataFromApi()
     }
     
     // Return the number of rows for the table.
@@ -33,10 +30,44 @@ class ViewController: UIViewController, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath) as! TodoItemCell
        
         // Configure the cell’s contents.
-        cell.itemDescription!.text = tableData[indexPath.row].description
-        cell.itemDone!.isOn = tableData[indexPath.row].isDone
+        cell.itemDescription!.text = tableData[indexPath.row].name
+        cell.itemDone!.isOn = tableData[indexPath.row].isComplete
            
         return cell
     }
     
+    private func getDataFromApi() {
+        let urlString = "https://absreimtodoapi.azurewebsites.net/api/TodoItems"
+        guard let url = URL(string: urlString) else {
+            fatalError("Error creating URL object when trying to GET data from API.")
+        }
+        let urlRequest = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: {
+            (data, response, error) in
+            guard error == nil else {
+                fatalError("Error encountered while making HTTP request.")
+            }
+            guard let httpResponse = response as? HTTPURLResponse else {
+                fatalError("Reponse returned from HTTP request is not of type HTTPURLResponse.")
+            }
+            guard httpResponse.statusCode == 200 else {
+                fatalError("Unexpected status code in reponse to GET request.")
+            }
+            guard data != nil else {
+                fatalError("No data in response to GET request.")
+            }
+            let decoder = JSONDecoder()
+            do {
+                let newTableData = try decoder.decode([TodoItem].self, from: data!)
+                self.tableData = newTableData
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            catch {
+                fatalError("Failed to decode data from API.")
+            }
+        })
+        task.resume()
+    }
 }
